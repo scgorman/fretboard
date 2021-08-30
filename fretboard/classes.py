b95e0189cf02df35d80faf19e5819618d8d804fb
MrSeanGorman <12507045+MrSeanGorman@users.noreply.github.com>
2021-08-30 02:31:12 -0500

    Continued work on classes.py

--- fretboard/classes.py
+++ fretboard/classes.py
@@ -1,24 +1,83 @@
-### Class module
+# Class module
+
+note_circle = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+
 
 class Guitar:
 
     def __init__(self, guitar_string_count, guitar_string_pitches, fretboard_length):
         self.guitar_string_count = guitar_string_count
-        self.guitar_string_tunings = guitar_string_pitches
+        self.guitar_string_pitches = guitar_string_pitches
         self.fretboard_length = fretboard_length
 
-    @classmethod
-    def set_guitar_string_count(cls, count):
-        guitar_string_count = count
+    def set_guitar_string_count(self, count):
+        self.guitar_string_count = count
 
-    @classmethod
-    def set_guitar_string_pitches(cls, pitches):
-        guitar_string_pitches = pitches
+    def set_guitar_string_pitches(self, pitches):
+        self.guitar_string_pitches = pitches
 
-    @classmethod
-    def set_fretboard_length(cls, length):
-        fretboard_length = length
+    def set_fretboard_length(self, length):
+        self.fretboard_length = length
+
+    def get_guitar_string_count(self):
+        return self.guitar_string_count
+
+    def get_guitar_string_pitches(self):
+        return self.guitar_string_pitches
+
+    def get_fretboard_length(self):
+        return self.fretboard_length
+
+
+class GuitarString:
+
+    def __init__(self, pitch, fretboard_length, notes):
+        self.pitch = pitch
+        self.fretboard_length = fretboard_length
+        self.notes = notes
+
+    def calculate_notes(self, pitch, fretboard_length):
+
+        notes = []
+        initial_position = int(note_circle.index(pitch))
+
+        notes.append(note_circle[initial_position])
+        current_position = initial_position + 1
+
+        for fret in range(fretboard_length):
+
+            notes.append(note_circle[current_position])
+
+            if current_position == 11:
+                current_position = 0
+            else:
+                current_position += 1
+
+        self.notes = notes
+
+    def set_pitch(self, pitch):
+        self.pitch = pitch
+
+    def set_fretboard_length(self, length):
+        self.fretboard_length = length
+
+    def get_pitch(self):
+        return self.pitch
+
 
 class Fretboard:
 
-    def __init__(self, ):
+    def __init__(self, guitar_strings):
+        self.guitar_strings = guitar_strings
+
+    def set_guitar_strings(self, guitar_strings):
+        self.guitar_strings = guitar_strings
+
+    def get_guitar_strings(self):
+        return self.guitar_strings
+
+
+class Visualizer:
+
+    def __init__(self, fretboard_notes):
+        self.fretboard_notes = fretboard_notes
